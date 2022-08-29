667c03a71286826bcc2cde9880a580d23b1fd988
fyp <15693476962@163.com>
2022-08-29 18:09:51 +0800

    .github

--- emailHtml.js
+++ emailHtml.js
@@ -1,57 +1,120 @@
-function fn(weatherData, lifeData, word, imgurl, lovingDays) {
+const dayjs = require('dayjs');
+const hour  =dayjs().format('HH')
+
+function fn(
+    weatherData, lifeData, imgurl, lovingDays,
+    content,source,
+    content1,
+    note,source2,content2,
+    content3,
+    content4,
+    content5,
+) {
   const { daily: weatherDataDaily } = weatherData;
   const { daily } = lifeData;
-
   return `<!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/qweather-icons@1.1.1/font/qweather-icons.css">
     </head>
+    <style>
+    .daily_item{
+        width: 250px;
+        min-height:200px ;
+        background: #fff;
+        border-radius: 12px;
+        margin: 20px 0;
+    }
+    </style>
     <body>
+    <!-- 天气 -->
+    <div style="margin: 10px 0;font-size: 20px;font-weight: bold">今天是${dayjs().format('YYYY-MM-DD')}</div>
+    <div style="margin: 10px 0">星期${['天', '一', '二', '三', '四', '五', '六'][dayjs().day()]}</div>
+    
+    <div class="daily_item">
+                <div>温度：${weatherDataDaily[0].tempMin}°C -- ${weatherDataDaily[0].tempMax}°C</div>
+                <div>日升：${weatherDataDaily[0].sunrise}</div>
+                <div>日落：${weatherDataDaily[0].sunset}</div>
+                <div>天气：${weatherDataDaily[0].textDay} <i class="qi-${weatherDataDaily[0].iconDay}"></i></div>
+                <div>风向：${weatherDataDaily[0].windDirDay} </div>
+                <div>风力：${weatherDataDaily[0].windScaleDay} </div>
+                <div>风速：${weatherDataDaily[0].windSpeedDay} </div>
+                <div>相对湿度：${weatherDataDaily[0].humidity} </div>
+            </div>
       <div>
+      <p style="margin: 20px 0;font-size: 20px">
+        <font color="#0066FF">空</font><font color="#3FA5FF">气</font><font color="#0066FF">指</font><font color="#3FA5FF">数</font>
+    </p>
+        <ul style="font-size: 18px;margin: 20px 0">
+            <li style="margin: 10px 0">${daily.filter(e=>e.type=='1')[0]['name']}：${daily.filter(e=>e.type=='1')[0]['text']}</li>
+            <li style="margin: 10px 0">${daily.filter(e=>e.type=='3')[0]['name']}：${daily.filter(e=>e.type=='3')[0]['text']}</li>
+            <li style="margin: 10px 0">${daily.filter(e=>e.type=='5')[0]['name']}：${daily.filter(e=>e.type=='5')[0]['text']}</li>
+            <li style="margin: 10px 0">${daily.filter(e=>e.type=='8')[0]['name']}：${daily.filter(e=>e.type=='8')[0]['text']}</li>
+            <li style="margin: 10px 0">${daily.filter(e=>e.type=='11')[0]['name']}：${daily.filter(e=>e.type=='11')[0]['text']}</li>
+            <li style="margin: 10px 0">${daily.filter(e=>e.type=='15')[0]['name']}：${daily.filter(e=>e.type=='15')[0]['text']}</li>
+        </ul>
         <!-- 天数 -->
-        <div>
-          <p>今天是在一起的第${lovingDays}天！</p>
-        </div>
-        <!-- 图片 -->
-        <div>
+          <p style="margin:10px 0;font-size: 20px">
+            <font color="#FF00FF">今</font><font color="#ED00FF">天</font><font color="#DB00FF">是</font><font color="#C900FF">和</font><font color="#B700FF">你</font><font color="#A500FF">在</font><font color="#9300FF">一</font><font color="#8100FF">起</font><font color="#6F00FF">的</font><font color="#E000FF">${lovingDays}</font><font color="#1500FF">天</font>
+          </p>
           <img
-            style="width: 100%; max-width: 768px"
+            style="width: 100%; max-width: 768px;border-radius: 12px;margin-bottom: 15px;"
             src="${imgurl}"
             alt="图片"
           />
-        </div>
-        <!-- 每日一句 -->
+        <!-- 获取朋友圈的简单走心的文案句子 -->
+          <p style="font-size: 18px; text-indent: 2em;">
+            ${content}
+          </p>
+          <p style="font-size: 16px; text-indent: 2em;">
+            <span style="float: right"> ---- ${source}</span>
+          </p>
+        <!-- 喜欢一个人，连TA放的屁都是彩虹色的 -->
+        <p style="font-size: 20px;">
+          <font color="#FF2800">爱</font><font color="#FF5000">一</font><font color="#FF7800">个</font><font color="#FFA000">人</font><font color="#FFC800">,</font><font color="#FFF000">翻</font><font color="#D7FF00">一</font><font color="#AFFF00">座</font><font color="#87FF00">山</font><font color="#5FFF00">.</font><font color="#37FF00">.</font><font color="#0FFF00">.</font>
+          </p>
+          <p style="font-size: 18px; text-indent: 2em; font-style: italic;">
+           ${content1}
+          </p>
+        <!-- 接口返回随机一句英剧句子，包含英语原句、释义、来源等。 -->
         <div>
-          <p style="font-size: 14px; text-indent: 2em; font-style: italic;">
-            ${word}
+            <p style="font-size: 20px;">
+           <font color="#33CCFF">每</font><font color="#728CFF">日</font><font color="#334CFF">英</font><font color="#720CFF">语</font>
+          </p>
+          <p style="font-size:18px; text-indent: 2em; font-style: italic;">
+           ${content2}
+          </p>
+          <p style="font-size: 18px; text-indent: 2em; font-style: italic;">
+           ${note}
+          </p>
+          <p style="font-size: 18px; text-indent: 2em; font-style: italic;">
+           ${source2}
           </p>
         </div>
-        <!-- 天气 -->
+        <!-- 网络云音乐App上，那些脍炙人口的热门评论。 -->
         <div>
-          <p>
-            <b>今日气温:</b>
-            <span>${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C</span>
+        <p style="font-size: 20px;">
+           <font color="#FF2800">网</font><font color="#FF5000">易</font><font color="#FF7800">云</font><font color="#FFA000">热</font><font color="#FFC800">评</font>
           </p>
-          <ul>
-            <li style="margin-bottom: 10px">
-              ${daily[1].name}(${daily[1].category}):
-              ${daily[1].text}
-            </li>
-            <li style="margin-bottom: 10px">
-              ${daily[2].name}(${daily[2].category}):
-              ${daily[2].text}
-            </li>
-            <li style="margin-bottom: 10px">
-              ${daily[0].name}(${daily[0].category}):
-              ${daily[0].text}
-            </li>
-          </ul>
+          <p style="font-size: 18px; text-indent: 2em; font-style: italic;">
+           ${content3}
+          </p>
         </div>
+        <div>
+                <p style="font-size: 20px;">
+                 <font color="#FF3333">有</font><font color="#FF7272">你</font><font color="#FF33B2">真</font><font color="#FF72F2">好</font>
+              </p>
+              <p style="font-size: 16px; text-indent: 2em;">
+               ${hour > 12 && hour < 24 ? content5 : content4}
+              </p>
+        </div>
+        <div>
       </div>
-    </body>
+    </div></body>
+    
   </html>
   `;
 }
--- index.js
+++ index.js
@@ -41,31 +41,89 @@ async function init() {
       `http://api.tianapi.com/txapi/one/index?key=${tianXingKey}`
     );
     const oneData = await oneRes.json();
-    const { word, imgurl } = oneData.newslist[0];
+    const {  imgurl } = oneData.newslist[0];
+    // 获取朋友圈的简单走心的文案句子
+    const twoRes = await fetch(
+      `http://api.tianapi.com/pyqwenan/index?key=${tianXingKey}`
+    );
+    const twoData = await twoRes.json();
+    const { source, content } = twoData.newslist[0];
+    // 喜欢一个人，连TA放的屁都是彩虹色的
+    const threeRes = await fetch(
+      `http://api.tianapi.com/caihongpi/index?key=${tianXingKey}`
+    );
+    const threeData = await threeRes.json();
+    const {  content:content1 } = threeData.newslist[0];
+    // 接口返回随机一句英剧句子，包含英语原句、释义、来源等。
+    const fourRes = await fetch(
+      `http://api.tianapi.com/everyday/index?key=${tianXingKey}`
+    );
+    const fourData = await fourRes.json();
+    const { source:source2 , content:content2,note } = fourData.newslist[0];
+    // 网络云音乐App上，那些脍炙人口的热门评论。
+    const fiveRes = await fetch(
+      `http://api.tianapi.com/everyday/index?key=${tianXingKey}`
+    );
+    const fiveData = await fiveRes.json();
+    const {content:content3 } = fiveData.newslist[0];
+
+    // 早安。
+    const sixRes = await fetch(
+      `http://api.tianapi.com/zaoan/index?key=${tianXingKey}`
+    );
+    const sixData = await sixRes.json();
+    const { content:content4 } = sixData.newslist[0];
+    // 晚安。
+    const sevenRes = await fetch(
+      `http://api.tianapi.com/wanan/index?key=${tianXingKey}`
+    );
+    const sevenData = await sevenRes.json();
+    const { content:content5 } = sevenData.newslist[0];
+    // 土味情话。
+    const eightRes = await fetch(
+      `http://api.tianapi.com/saylove/index?key=${tianXingKey}`
+    );
+    const eightData = await eightRes.json();
+    const { content:content6 } = eightData.newslist[0];
 
     // 计算日期
-    const lovingDays = dayjs(dayjs().tz('Asia/Shanghai')).diff(
-      startDay,
-      'days'
-    );
 
-    // 用邮件模版生成字符串
-    const htmlStr = emailHtml(weatherData, lifeData, word, imgurl, lovingDays);
+
 
     // 发送邮件;
-    sendEmail({
-      from: fromDisplayText,
-      to,
-      subject: fromDisplaySubText,
-      html: htmlStr,
-    });
+    for (const toItem in to) {
+      console.log(toItem)
+      let lovingDays = dayjs(dayjs().tz('Asia/Shanghai')).diff(
+        startDay[toItem],
+        'days'
+      );
+      // 用邮件模版生成字符串
+      const htmlStr = emailHtml(
+        weatherData, lifeData,
+        imgurl,
+        lovingDays,
+        content,source,
+        content1,
+        note,source2,content2,
+        content3,
+        content4,
+        content5,
+      );
+      sendEmail({
+        from: fromDisplayText,
+        to:to[toItem],
+        subject:content6 ,
+        html: htmlStr,
+      });
+    }
   } catch (e) {
+    console.log(e)
     // 发送邮件给自己提示
     sendEmail({
       from: '报错啦',
       to: user,
       subject: '定时邮件-报错提醒',
-      html: '请查看github actions',
+      html: '我知道你很急，但是你先别急',
     });
   }
 }
