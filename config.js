8e4108aabbed9e67fb2372e8a83ece94334d4f38
xi-an-studen <68893255+xi-an-studen@users.noreply.github.com>
2022-08-29 10:26:22 +0800

    Add files via upload
    first

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '冯亚鹏', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+  user: '1364117434@qq.com', // 发送者邮箱
+  pass: 'kncetoaqoqxgfhdi', // 发送者邮箱MTP协议密码
+  to: '1364117434@qq.com', // 发送到谁，填邮箱
+  weatherKey: 'b65c7baffe744fc4b28c11fc6f214dae', // 和风天气key
+  location: '101110101',  // 和风天气-成都武侯区的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: '72e5e9aefed4f9498fd8e43381298857', // 天行数据的key
+  startDay: '2018-10-25', // 在一起的日期
+}
